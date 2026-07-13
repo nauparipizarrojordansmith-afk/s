69d2ec3aab20c06040d547f4e322b2b0264bdf31
nauparipizarrojordansmith-afk <nauparipizarrojordansmith@gmail.com>
2026-07-13 15:37:36 -0500

    Add files via upload

---
+++ DiasSemana.java
@@ -0,0 +1,9 @@
+class DiasSemana {
+    public static void main(String[] args) {
+        String[] dias = {"LUN", "MAR", "MIE", "JUE","VIE","SAB","DOM"};
+
+        for(int indice=0; indice<dias.length; indice++){
+            System.out.println( dias[indice] );
+        }
+    }
+}
---
+++ ForMejorado.java
@@ -0,0 +1,9 @@
+class ForMejorado {
+    public static void main(String[] args) {
+        String[] dias = {"LUN", "MAR", "MIE", "JUE","VIE","SAB","DOM"};
+
+        for( String dia : dias ){
+            System.out.println( dia );
+        }
+    }
+}
